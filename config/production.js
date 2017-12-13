776848646af30d593b1223f0daa38ac1bbe571f1
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-14 06:22:03 +0800

    Remove unused module

--- config/production.js
+++ config/production.js
@@ -1,13 +1,9 @@
 'use strict';
 
 /**
- * Development environment config of any configuration required to setup
+ * Production environment config of any configuration required to setup
  * the application which includes db, etc for developer purposes.
  */
-const path = require('path');
-const url = require('url');
-const sprintf = require('util').format;
 const defaults = require('./defaults');
-const openFile = require('fs').createWriteStream;
 
 module.exports = Object.assign({}, defaults, {});
