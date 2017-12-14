54c16fd69138f26ae1a178232572d3b7b2ed5af1
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-15 01:36:54 +0800

    Add SendGrid Api

--- build-heroku.sh
+++ build-heroku.sh
@@ -8,5 +8,6 @@ export PEERSVIEW_DB_PASSWORD='96fc507f'
 export PEERSVIEW_DATABASE='heroku_a0351cee4a00f2c'
 export PEERSVIEW_DB_PORT='3306'
 export PEERSVIEW_DB_TIMEZONE='+00:00'
+export SENDGRID_API_KEY='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'
 
 node index.js
