568ff5c8ff42745bfe02eb612b643e8f40ac49d8
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-20 06:22:01 +0800

    Add aditional optional params

--- handlers/community/get-community-posts.js
+++ handlers/community/get-community-posts.js
@@ -3,6 +3,18 @@
 /**
  * @author Jo-Ries Canino
  * @description Get List of Community Post
+ * Basically same userType
+ * and same userStudyLevel for student
+ *
+ * If the user is a professional he/she can browse
+ * any userStudyLevel
+ *
+ * If the post is a professional the student userType
+ * can see the post.
+ *
+ * It is also tied in our private community
+ * So reused the communityPost table
+ * and just add the communityId
  */
 
 const lib = require('../../lib');
@@ -19,25 +31,23 @@ const lib = require('../../lib');
 function validateParams (req, res, next) {
   let paramsSchema = {
     courseId: {
-      notEmpty: {
-        errorMessage: 'Missing Resource: Course Id'// not empty because we need to get the user in which course he/she belongs
-      },
-      isInt: {
-        errorMessage: 'Invalid Resource: Course Id'// not empty because we need to get the user in which course he/she belongs
-      }
-    }
-  };
-
-  let headerSchema = {
-    token: {
-      notEmpty: {
-        errorMessage: 'Missing Resource: Token'
-      }
+      optional: true // because to reuse the general tab and when the user browse in the courses(student); professionals can browse all
+    },
+    userStudyLevelId: {// when the professionals browse it so we need it as an optional params
+      optional: true
+    },
+    communityId: {// use for private communities
+      optional: true
+    },
+    offset: {
+      optional: true
+    },
+    limit: {
+      optional: true
     }
   };
 
   req.checkParams(paramsSchema);
-  req.checkHeaders(headerSchema);
   return req.getValidationResult()
   .then(validationErrors => {
     if (validationErrors.array().length !== 0) {
@@ -53,25 +63,122 @@ function validateParams (req, res, next) {
   });
 }
 
+/**
+ * This would be the fallback if the user existed
+ * @see {@link lib/isUserTokenExist}
+ * @see isUserTokenExist
+ * @param {any} req request object
+ * @param {any} res response object
+ * @param {any} next next object
+ * @returns {next} returns the next handler - success response
+ * @returns {rpc} returns the validation error - failed response
+ */
+function checkUserType (req, res, next) {
+  let user = req.$scope.user;
+  if (user.userTypeId) {
+    // check if the userTypeId data code='student'
+    return req.db.userType.findOne({
+      where: {
+        id: user.userTypeId
+      }
+    })
+    .then(userType => {
+      req.$scope.userType = userType;
+      next();
+      return userType;
+    })
+    .catch(error => {
+      res.status(500)
+      .send(new lib.rpc.InternalError(error));
+
+      req.log.error({
+        err: error
+      }, 'userType.findOne Error - get-community-posts');
+    });
+  } else {
+    return res.status(400).send({
+      status: 'ERROR',
+      status_code: 101,
+      status_message: 'Missing Resource: userTypeId',
+      http_code: 400
+    });
+  }
+}
+
+function getProfessionalsUserTypeId (req, res, next) {// eslint-disable-line id-length
+  return req.db.userType.findOne({
+    where: {
+      code: 'professionals'
+    }
+  })
+  .then(professionalsUserType => {//eslint-disable-line id-length
+    req.$scope.professionalsUserType = professionalsUserType;
+    next();
+    return professionalsUserType;
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+
+    req.log.error({
+      err: error
+    }, 'userType.findOne Error - get-community-posts');
+  });
+}
+
 function getCommunityPosts (req, res, next) {
   let user = req.$scope.user;
+  let userType = req.$scope.userType;
+  let professionalsUserType = req.$scope.professionalsUserType;// eslint-disable-line id-length
   let courseId = req.$params.courseId;
+  let userStudyLevelId = req.$params.userStudyLevelId;
+  let communityId = req.$params.communityId;
+  let offset = req.$params.offset;
+  let limit = req.$params.limit;
   const sequelize = req.db.communityPostRating.sequelize;
-  const colRating = sequelize.col(['communityPostRating', 'rating'].join('.'));
+  const colRating = sequelize.col('communityPostRating.rating');
   const colAVG = sequelize.fn('AVG', colRating);
+  let where = {// meaning professionals can access it;
+    courseId: typeof courseId === 'undefined' ? null : courseId,
+    userStudyLevelId: typeof userStudyLevelId === 'undefined' ? null : userStudyLevelId,
+  };
+
+  if (userType.code === 'student' && (typeof communityId === 'undefined')) {// get also the post of a professionals
+    where = {
+      [req.Op.and]: {
+        [req.Op.or]: [{
+          userStudyLevelId: user.userStudyLevelId
+        }, {
+          userTypeId: professionalsUserType.id// this is where we get the post from the professionals
+        }],
+        communityId: null,
+        courseId: typeof courseId === 'undefined' ? null : courseId
+      }
+    };
+  } else if (typeof communityId !== 'undefined') {// we are on the private community request
+    where = {
+      communityId: {
+        [req.Op.eq]: communityId
+      }
+    };
+  }
 
   return req.db.communityPost.findAll({
     attributes: [
       'message',
+      'createdAt',
       [sequelize.fn('ROUND', colAVG, 2), 'roundedRating'],
       [sequelize.fn('COUNT',
         sequelize.col(['communityPostRating', 'userId'].join('.'))), 'ratingCount'],
       [sequelize.fn('COUNT',
         sequelize.col(['communityPostLike', 'userId'].join('.'))), 'likeCount'],
       [sequelize.fn('COUNT',
-        sequelize.col(['communityPostPageview', 'userId'].join('.'))), 'pageviewCount'],
+        sequelize.col(['communityPostPageview', 'userId'].join('.'))), 'pageviewCount']
     ],
     include: [{
+      model: req.db.user,
+      attributes: ['id', 'firstName', 'lastName', 'email']
+    }, {
       model: req.db.communityPostRating,
       as: 'communityPostRating',
       attributes: []
@@ -86,20 +193,17 @@ function getCommunityPosts (req, res, next) {
     }, {
       model: req.db.communityPostReply,
       as: 'communityPostReply',
-      attributes: ['comment'],
+      attributes: ['comment', 'createdAt'],
       include: [{
         model: req.db.user,
         attributes: ['id', 'firstName', 'lastName', 'email']
       }]
     }],
     group: ['communityPost.id'],
-    where: {
-      [req.Op.and]: {
-        userTypeId: user.userTypeId,
-        userStudyLevelId: user.userStudyLevelId,
-        courseId: courseId
-      }
-    }
+    where: where,
+    subQuery: false,
+    offset: !offset ? 0 : offset,
+    limit: !limit ? 10 : limit
   })
   .then(communityPosts => {
     req.$scope.communityPosts = communityPosts;
@@ -135,5 +239,7 @@ function response (req, res) {
 }
 
 module.exports.validateParams = validateParams;
+module.exports.checkUserType = checkUserType;
+module.exports.getProfessionalsUserTypeId = getProfessionalsUserTypeId;
 module.exports.logic = getCommunityPosts;
 module.exports.response = response;
