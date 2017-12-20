1e51c341995243c3d4f7d3679ccd64d90d668b4e
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-20 21:42:37 +0800

    Reorganize function call

--- routes-api/interest.js
+++ routes-api/interest.js
@@ -4,13 +4,16 @@ const handlers = require('../handlers');
 const lib = require('../lib');
 
 function interestApi (apiRouter) {
-  apiRouter.get('/interest',
+  apiRouter.get('/interest/:interestCategoryId',
     lib.params,
+    lib.isTokenExist.user,
     handlers.getInterest.validateParams,
     handlers.getInterest.logic,
     handlers.getInterest.response);
 
-  apiRouter.get('/interest/category',
+  apiRouter.get('/interest',
+    lib.params,
+    lib.isTokenExist.user,
     handlers.getInterestCategory.logic,
     handlers.getInterestCategory.response);
 }
