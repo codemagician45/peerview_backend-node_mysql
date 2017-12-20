6cb9185fa9f944fde7750037a90eafb54299dde9
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-20 11:52:20 +0800

    Add community_users table

---
+++ data-models/community-users.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * This would be creation of private community
+ */
+
+module.exports = function (sequelize, dataTypes) {
+  const CommunityUsers = sequelize.define('communityUsers', {
+    id: {
+      type: dataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    status: {// meaning the user has not yet approved the request
+      type: dataTypes.STRING,
+      defaultValue: 'pending'
+    },
+    institutionName: {
+      type: dataTypes.STRING
+    }
+  }, {
+    tableName: 'community_users',
+    timestamp: true,
+    collate: 'utf8_unicode_ci',
+    indexes: []
+  });
+
+  return CommunityUsers;
+};
