1cb3795f55bf08306218677ee42adcf0e4efd832
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-21 14:07:21 +0800

    Add post_poll_option table

---
+++ data-models/post-poll-option.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function (sequelize, dataTypes) {
+  const PostPollOption = sequelize.define('postPollOption', {
+    id: {
+      type: dataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: dataTypes.STRING
+    }
+  }, {
+    tableName: 'post_poll_option',
+    timestamp: true,
+    collate: 'utf8_unicode_ci',
+    indexes: []
+  });
+
+  PostPollOption.associate = function (models) {
+    this.belongsTo(models.postPoll);
+  };
+
+  return PostPollOption;
+};
