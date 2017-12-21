f40f855a33a0e555bc9175b367061c82ceb8a59c
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-22 04:41:05 +0800

    Add a leisure post event description

--- routes-api/leisure.js
+++ routes-api/leisure.js
@@ -16,11 +16,24 @@ function leisureApi (apiRouter) {
     handlers.leisure.getEventTypes.logic,
     handlers.leisure.getEventTypes.response);
 
+  /**
+   * This one is a special case in which the upload.events
+   * precedes the lib.params
+   * This is because the content-type: multipart/form-data
+   * In which body parser don't handle that kind of content-type.
+   * That is we are using upload.events as a mediator and as well
+   * to use the capability of the multer to assemble our
+   * req.body and merge them in lib.params
+   */
   apiRouter.post('/event',
-    lib.params,
     lib.isTokenExist.user,
-    handlers.leisure.postEvent.validateParams,
     lib.upload.events,
+    lib.params,
+    handlers.leisure.postEvent.validateParams,
+    handlers.leisure.postEvent.logic,
+    handlers.leisure.postEvent.saveImages,
+    handlers.leisure.postEvent.savePosters,
+    handlers.leisure.postEvent.saveVideos,
     handlers.leisure.postEvent.response);
 }
 
