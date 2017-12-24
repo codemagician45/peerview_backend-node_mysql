257d76ced3644d711803f8ce773a441a42c1e65e
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-24 12:58:49 +0800

    Add /api/v1/campus/:campusId/freshers-feed

---
+++ data-models/campus-user-course-class.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function (sequelize, dataTypes) {
+  const CampusUserCourseClass = sequelize.define('campusUserCourseClass', {// eslint-disable-line id-length
+    id: {
+      type: dataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: dataTypes.STRING
+    }
+  }, {
+    tableName: 'campus_user_course_class',
+    timestamp: true,
+    collate: 'utf8_unicode_ci',
+    indexes: []
+  });
+
+  CampusUserCourseClass.associate = function (models) {
+    this.belongsTo(models.user);
+    this.belongsTo(models.campusCourseClass);
+  };
+
+  return CampusUserCourseClass;
+};
