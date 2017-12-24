64f09f96d1ed348fa0684e727ea18d5662b91f10
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-24 13:09:43 +0800

    Add related http routes for campus

--- routes-api/campus.js
+++ routes-api/campus.js
@@ -1,9 +1,233 @@
-'use strict';
+'use strict';/*eslint-disable max-statements*/
 
 const handlers = require('../handlers');
 const lib = require('../lib');
 
 function campusApi (apiRouter) {
+  apiRouter.get('/campus/:campusId/freshers-feed',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.getCampusFreshersFeed.validateParams,
+    handlers.campus.getCampusFreshersFeed.logic,
+    handlers.campus.getCampusFreshersFeed.response);
+
+  apiRouter.post('/campus/user/classes', // adding of classes of a user
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusUserCourseClasses.validateParams,
+    handlers.campus.postCampusUserCourseClasses.logic,
+    handlers.campus.postCampusUserCourseClasses.response);
+
+  apiRouter.post('/campus/:campusId/post',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPost.validateParams,
+    handlers.campus.postCampusPost.logic,
+    handlers.campus.postCampusPost.response);
+
+  apiRouter.post('/campus/:campusId/post/poll',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostPoll.validateParams,
+    handlers.campus.postCampusPostPoll.logic,
+    handlers.campus.postCampusPostPoll.saveCampusPostPollOption,
+    handlers.campus.postCampusPostPoll.response);
+
+  apiRouter.post('/campus/post/:postId/like',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostLike.validateParams,
+    handlers.campus.postCampusPostLike.logic,
+    handlers.campus.postCampusPostLike.response);
+
+  apiRouter.post('/campus/post/:postId/pageview',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostPageview.validateParams,
+    handlers.campus.postCampusPostPageview.logic,
+    handlers.campus.postCampusPostPageview.response);
+
+  apiRouter.post('/campus/post/:postId/rating',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostRating.validateParams,
+    handlers.campus.postCampusPostRating.logic,
+    handlers.campus.postCampusPostRating.response);
+
+  apiRouter.post('/campus/post/:postId/reply',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostReply.validateParams,
+    handlers.campus.postCampusPostReply.logic,
+    handlers.campus.postCampusPostReply.response);
+
+  apiRouter.post('/campus/post/:postId/report',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostReport.validateParams,
+    handlers.campus.postCampusPostReport.logic,
+    handlers.campus.postCampusPostReport.response);
+
+  // course feed
+  apiRouter.post('/campus/:campusId/course/:courseId/post',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPost.validateParams,
+    handlers.campus.postCampusPost.logic,
+    handlers.campus.postCampusPost.response);
+
+  apiRouter.post('/campus/:campusId/course/:courseId/post/poll',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostPoll.validateParams,
+    handlers.campus.postCampusPostPoll.logic,
+    handlers.campus.postCampusPostPoll.saveCampusPostPollOption,
+    handlers.campus.postCampusPostPoll.response);
+
+  apiRouter.post('/campus/course/post/:postId/reply',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostReply.validateParams,
+    handlers.campus.postCampusPostReply.logic,
+    handlers.campus.postCampusPostReply.response);
+
+  apiRouter.post('/campus/course/post/:postId/like',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostLike.validateParams,
+    handlers.campus.postCampusPostLike.logic,
+    handlers.campus.postCampusPostLike.response);
+
+  apiRouter.post('/campus/course/post/:postId/pageview',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostPageview.validateParams,
+    handlers.campus.postCampusPostPageview.logic,
+    handlers.campus.postCampusPostPageview.response);
+
+  apiRouter.post('/campus/course/post/:postId/rating',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostRating.validateParams,
+    handlers.campus.postCampusPostRating.logic,
+    handlers.campus.postCampusPostRating.response);
+
+  apiRouter.post('/campus/course/post/:postId/report',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostReport.validateParams,
+    handlers.campus.postCampusPostReport.logic,
+    handlers.campus.postCampusPostReport.response);
+
+  // freshers feed
+  apiRouter.post('/campus/:campusId/freshers-feed/:freshersFeedId/post',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPost.validateParams,
+    handlers.campus.postCampusPost.logic,
+    handlers.campus.postCampusPost.response);
+
+  apiRouter.post('/campus/:campusId/freshers-feed/:freshersFeedId/post/poll',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostPoll.validateParams,
+    handlers.campus.postCampusPostPoll.logic,
+    handlers.campus.postCampusPostPoll.saveCampusPostPollOption,
+    handlers.campus.postCampusPostPoll.response);
+
+  apiRouter.post('/campus/freshers-feed/post/:postId/reply',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostReply.validateParams,
+    handlers.campus.postCampusPostReply.logic,
+    handlers.campus.postCampusPostReply.response);
+
+  apiRouter.post('/campus/freshers-feed/post/:postId/like',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostLike.validateParams,
+    handlers.campus.postCampusPostLike.logic,
+    handlers.campus.postCampusPostLike.response);
+
+  apiRouter.post('/campus/freshers-feed/post/:postId/pageview',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostPageview.validateParams,
+    handlers.campus.postCampusPostPageview.logic,
+    handlers.campus.postCampusPostPageview.response);
+
+  apiRouter.post('/campus/freshers-feed/post/:postId/rating',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostRating.validateParams,
+    handlers.campus.postCampusPostRating.logic,
+    handlers.campus.postCampusPostRating.response);
+
+  apiRouter.post('/campus/freshers-feed/post/:postId/report',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostReport.validateParams,
+    handlers.campus.postCampusPostReport.logic,
+    handlers.campus.postCampusPostReport.response);
+
+  // class feed
+  apiRouter.post('/campus/:campusId/course/class/:classId/post',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPost.validateParams,
+    handlers.campus.postCampusPost.logic,
+    handlers.campus.postCampusPost.response);
+
+  apiRouter.post('/campus/:campusId/course/class/:classId/post/poll',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostPoll.validateParams,
+    handlers.campus.postCampusPostPoll.logic,
+    handlers.campus.postCampusPostPoll.saveCampusPostPollOption,
+    handlers.campus.postCampusPostPoll.response);
+
+  apiRouter.post('/campus/:campusId/course/class/:classId/post/brainstorming',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPost.validateParams,
+    handlers.campus.postCampusPost.logic,
+    handlers.campus.postCampusPost.response);
+
+  apiRouter.post('/campus/course/class/post/:postId/reply',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostReply.validateParams,
+    handlers.campus.postCampusPostReply.logic,
+    handlers.campus.postCampusPostReply.response);
+
+  apiRouter.post('/campus/course/class/post/:postId/like',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostLike.validateParams,
+    handlers.campus.postCampusPostLike.logic,
+    handlers.campus.postCampusPostLike.response);
+
+  apiRouter.post('/campus/course/class/post/:postId/pageview',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostPageview.validateParams,
+    handlers.campus.postCampusPostPageview.logic,
+    handlers.campus.postCampusPostPageview.response);
+
+  apiRouter.post('/campus/course/class/post/:postId/rating',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostRating.validateParams,
+    handlers.campus.postCampusPostRating.logic,
+    handlers.campus.postCampusPostRating.response);
+
+  apiRouter.post('/campus/course/class/post/:postId/report',
+    lib.params,
+    lib.isTokenExist.user,
+    handlers.campus.postCampusPostReport.validateParams,
+    handlers.campus.postCampusPostReport.logic,
+    handlers.campus.postCampusPostReport.response);
+
   apiRouter.get('/campus/jobs',
     lib.params,
     handlers.campus.getCampusJobs.logic,
