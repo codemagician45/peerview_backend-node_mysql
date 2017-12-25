24c76cff0887d1f16e0da43693f33dcf4f1a80d3
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-26 05:51:26 +0800

    Add POST /api/v1/campus/:campusId/student-group

---
+++ handlers/campus/post-campus-student-group.js
@@ -0,0 +1,122 @@
+'use strict';
+
+/**
+ * @author Jo-Ries Canino
+ * @description Post Campus Student Group
+ */
+
+const lib = require('../../lib');
+
+/**
+ * Validation of req.body, req, param,
+ * and req.query
+ * @param {any} req request object
+ * @param {any} res response object
+ * @param {any} next next object
+ * @returns {next} returns the next handler - success response
+ * @returns {rpc} returns the validation error - failed response
+ */
+function validateParams (req, res, next) {
+  let paramsSchema = {
+    campusId: {
+      isInt: {
+        errorMessage: 'Invalid Resource: Campus Id'
+      }
+    }
+  };
+
+  let bodySchema = {
+    campusPrivacyId: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Campus Privacy Id'
+      },
+      isInt: {
+        errorMessage: 'Invalid Resource: Campus Privacy Id'
+      }
+    },
+    name: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Name'
+      }
+    },
+    description: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Description'
+      }
+    },
+    adminEmail: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Admin Email'
+      },
+      isEmail: {
+        errorMessage: 'Invalid Resource: Admin Email'
+      }
+    }
+  };
+
+  req.checkParams(paramsSchema);
+  req.checkBody(bodySchema);
+  return req.getValidationResult()
+  .then(validationErrors => {
+    if (validationErrors.array().length !== 0) {
+      return res.status(400)
+      .send(new lib.rpc.ValidationError(validationErrors.array()));
+    }
+
+    return next();
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+  });
+}
+
+function postCampusStudentGroup (req, res, next) {// eslint-disable-line id-length
+  let user = req.$scope.user;
+  let campusId = req.$params.campusId;
+  let campusPrivacyId = req.$params.campusPrivacyId;
+  let name = req.$params.name;
+  let description = req.$params.description;
+  let adminEmail = req.$params.adminEmail;
+
+  return req.db.campusStudentGroup.create({
+    userId: user.id,
+    campusId: campusId,
+    campusPrivacyId: campusPrivacyId,
+    name: name,
+    description: description,
+    adminEmail: adminEmail
+  })
+  .then(campusStudentGroup => {
+    next();
+    return campusStudentGroup;
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+
+    req.log.error({
+      err: error.message
+    }, 'campusStudentGroup.create Error - post-campus-student-group');
+  });
+}
+
+/**
+ * Response data to client
+ * @param {any} req request object
+ * @param {any} res response object
+ * @returns {any} body response object
+ */
+function response (req, res) {
+  let body = {
+    status: 'SUCCESS',
+    status_code: 0,
+    http_code: 201
+  };
+
+  res.status(201).send(body);
+}
+
+module.exports.validateParams = validateParams;
+module.exports.logic = postCampusStudentGroup;
+module.exports.response = response;
