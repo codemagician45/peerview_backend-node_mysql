3461ea6c51bec9e8ed9166235aaaec757925b7f0
Jo-Ries Canino <canino_jories@hotmail.com>
2017-12-27 20:45:21 +0800

    Make the message field as TEXT type

--- data-models/community-post.js
+++ data-models/community-post.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const validator = require('validator');
+
 module.exports = function (sequelize, dataTypes) {
   const CommunityPost = sequelize.define('communityPost', {
     id: {
@@ -7,8 +9,35 @@ module.exports = function (sequelize, dataTypes) {
       primaryKey: true,
       autoIncrement: true
     },
-    message: {
+    message: {// we use this in the brainstorming that is why we saved the data in json format
+      type: dataTypes.TEXT,
+      allowNull: true,
+      get: function () {
+        var rawValue = this.getDataValue('message');
+        if (!rawValue) {
+          return (undefined);
+        } else if (validator.isJSON(rawValue)){
+          return JSON.parse(rawValue);
+        } else {
+          return rawValue;
+        }
+      },
+      set: function (val) {
+        var json = (typeof val === 'string') ? val : JSON.stringify(val);
+        this.setDataValue('message', json);
+      }
+    },
+    title: {
       type: dataTypes.STRING
+    },
+    description: {
+      type: dataTypes.STRING
+    },
+    question: {
+      type: dataTypes.STRING
+    },
+    duration: {
+      type: dataTypes.INTEGER
     }
   }, {
     tableName: 'community_post',
@@ -23,6 +52,7 @@ module.exports = function (sequelize, dataTypes) {
     this.belongsTo(models.userStudyLevel);
     this.belongsTo(models.course);
     this.belongsTo(models.community); // if this field has been filed the post belongsTo private community
+    this.hasMany(models.communityPostPollOption);
     this.hasMany(models.communityPostLike, {
       as: 'communityPostLike'
     });
