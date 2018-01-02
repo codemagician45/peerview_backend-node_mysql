ff09d1b6a967778349327a08b0adaafac8941d94
Jo-Ries Canino <canino_jories@hotmail.com>
2018-01-03 05:07:39 +0800

    Adding a sendEmail middleware

--- handlers/community/update-community-post.js
+++ handlers/community/update-community-post.js
@@ -18,21 +18,74 @@ const lib = require('../../lib');
  * @returns {rpc} returns the validation error - failed response
  */
 function validateParams (req, res, next) {
-  let paramsSchema = {
-    communityPostId: {
-      isInt: {
-        errorMessage: 'Invalid Resource: Community Post Id'
-      }
-    }
-  };
+  let isCareerUrl = req.route.path.indexOf('community/post/:communityPostId/career');
+  let isPostPollUrl = req.route.path.indexOf('community/post/:communityPostId/poll');
+  let paramsCommunityPost = req.route.path.indexOf(':communityPostId');
+  let bodySchema = {};
+  let paramsSchema = {};
 
-  let bodySchema = {
-    message: {
-      notEmpty: {
-        errorMessage: 'Missing Resource: message'
+  if (paramsCommunityPost !== -1) {
+    paramsSchema = {
+      communityPostId: {
+        isInt: {
+          errorMessage: 'Invalid Resource: Community Post Id'
+        }
+      },
+    };
+  }
+
+  if (isCareerUrl !== -1) {
+    bodySchema = {
+      title: {
+        notEmpty: {
+          errorMessage: 'Missing Resource: Title'
+        }
+      },
+      description: {
+        notEmpty: {
+          errorMessage: 'Missing Resource: Description'
+        }
       }
-    }
-  };
+    };
+  } else if (isPostPollUrl !== -1) {
+    bodySchema = {
+      question: {
+        notEmpty: {
+          errorMessage: 'Missing Resource: Question'
+        }
+      },
+      options: {
+        isArrayNotEmpty: {
+          errorMessage: 'Missing Resource: Options'
+        },
+        isArray: {
+          errorMessage: 'Invalid Resource: Options'
+        }
+      },
+      duration: {
+        notEmpty: {
+          errorMessage: 'Missing Resource: Duration'
+        },
+        isInt: {
+          errorMessage: 'Invalid Resource: Duration'
+        }
+      }
+    };
+  } else {
+    bodySchema = {
+      courseId: {// optional because the professionals can post/and courses related post
+        optional: true,
+        isInt: {
+          errorMessage: 'Invalid Resource: Course Id'
+        }
+      },
+      message: {
+        notEmpty: {
+          errorMessage: 'Missing Resource: Message'
+        }
+      }
+    };
+  }
 
   req.checkParams(paramsSchema);
   req.checkBody(bodySchema);
@@ -51,12 +104,37 @@ function validateParams (req, res, next) {
   });
 }
 
-function updateCommunityPost (req, res, next) {// eslint-disable-line id-length
-  let communityPostId = req.$params.communityPostId;
+/**
+ * This would be the fallback if the user existed
+ * In which if the user is still unverified
+ * @see {@link lib/isUserTokenExist}
+ * @see isUserTokenExist
+ * @param {any} req request object
+ * @param {any} res response object
+ * @param {any} next next object
+ * @returns {next} returns the next handler - success response
+ * @returns {rpc} returns the validation error - failed response
+ */
+function updateCommunityPost (req, res, next) {
+  let user = req.$scope.user;
+  let courseId = req.$params.courseId;
   let message = req.$params.message;
+  let title = req.$params.title;
+  let description = req.$params.description;
+  let question  = req.$params.question;
+  let duration = req.$params.duration;
+  let communityPostId = req.$params.communityPostId;
 
   return req.db.communityPost.update({
-    message: message
+    userId: user.id,
+    userTypeId: user.userTypeId,
+    userStudyLevelId: user.userStudyLevelId, // general community is tied with this one
+    courseId: courseId,
+    message: message,
+    title: title,
+    description: description,
+    question: question,
+    duration: duration
   }, {
     where: {
       id: {
@@ -74,7 +152,70 @@ function updateCommunityPost (req, res, next) {// eslint-disable-line id-length
 
     req.log.error({
       err: error.message
-    }, 'communityPost.update Error - post-community-post');
+    }, 'communityPost.update Error - update-community-post');
+  });
+}
+
+function removeCommunityPostPollOption (req, res, next) {// eslint-disable-line id-length
+  let communityPostId = req.$params.communityPostId;
+
+  return req.db.communityPostPollOption.destroy({
+    where: {
+      communityPostId: {
+        [req.Op.eq]: communityPostId
+      }
+    }
+  })
+  .then(communityPostPollOption => {// eslint-disable-line id-length
+    next();
+    return communityPostPollOption;
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+
+    req.log.error({
+      err: error.message
+    }, 'communityPostPollOption.destroy Error - update-community-post');
+  });
+}
+
+/**
+ * Save the options in the pollOption table
+ * @param {any} req request object
+ * @param {any} res response object
+ * @param {any} next next object
+ * @returns {next} returns the next handler - success response
+ * @returns {rpc} returns the validation error - failed response
+ */
+function saveCommunityPostPollOption (req, res, next) {// eslint-disable-line id-length
+  let communityPostId = req.$params.communityPostId;
+  let options = req.$params.options;
+  let question = req.$params.question;
+  let communityPostPollOption = [];// eslint-disable-line id-length
+
+  // check if we have params for question
+  if (!question) {return next();}
+
+  options.forEach(option => {
+    communityPostPollOption.push({
+      name: option,
+      communityPostId: communityPostId
+    });
+  });
+
+  return req.db.communityPostPollOption.bulkCreate(communityPostPollOption)
+  .then(communityPostPollOption => {// eslint-disable-line id-length
+    next();
+    return communityPostPollOption;
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+
+    req.log.error({
+      err: error.message
+    }, 'communityPostPollOption.bulkCreate Error - update-community-post');
   });
 }
 
@@ -96,4 +237,6 @@ function response (req, res) {
 
 module.exports.validateParams = validateParams;
 module.exports.logic = updateCommunityPost;
+module.exports.removeCommunityPostPollOption = removeCommunityPostPollOption;
+module.exports.saveCommunityPostPollOption = saveCommunityPostPollOption;
 module.exports.response = response;
