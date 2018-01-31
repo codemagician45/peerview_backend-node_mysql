5b71f8a19107332b5eb79aa5032ca2733b906790
Jo-Ries Canino <canino_jories@hotmail.com>
2018-01-31 21:21:12 +0800

    Add /api/v1/user/accomplishments endpoint

---
+++ handlers/user/update-user-accomplishments.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/**
+ * @author Jo-Ries Canino
+ * @description PUT User Accomplishments
+ */
+
+const lib = require('../../lib');
+
+/**
+ * Validation of req.body, req, param,
+ * and req.query
+ * @param {any} req request object
+ * @param {any} res response object
+ * @param {any} next next object
+ * @returns {next} returns the next handler - success response
+ * @returns {rpc} returns the validation error - failed response
+ */
+function validateParams (req, res, next) {
+  let bodySchema = {
+    accomplishments: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Accomplishments'
+      }
+    }
+  };
+
+
+  req.checkBody(bodySchema);
+  return req.getValidationResult()
+  .then(validationErrors => {
+    if (validationErrors.array().length !== 0) {
+      return res.status(400)
+      .send(new lib.rpc.ValidationError(validationErrors.array()));
+    }
+
+    return next();
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+  });
+}
+
+function updateUserAccomplishments (req, res, next) {// eslint-disable-line id-length
+  let user = req.$scope.user;
+  let accomplishments = {accomplishments: req.$params.accomplishments};
+
+  req.db.user.update(accomplishments, {
+    where: {
+      id: user.id
+    }
+  })
+  .then(user => {
+    next();
+    return user;
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+
+    req.log.error({
+      err: error.message
+    }, 'user.update Error - update-user-accomplishments');
+  });
+}
+
+/**
+ * Response data to client
+ * @param {any} req request object
+ * @param {any} res response object
+ * @returns {any} body response object
+ */
+function response (req, res) {
+  let body = {
+    status: 'SUCCESS',
+    status_code: 0,
+    http_code: 200
+  };
+
+  res.status(200).send(body);
+}
+
+module.exports.validateParams = validateParams;
+module.exports.logic = updateUserAccomplishments;
+module.exports.response = response;
