9e9869f655572948f50431d5cf7a2921ef9722cd
Jo-Ries Canino <canino_jories@hotmail.com>
2018-04-02 09:24:19 +0800

    Add handlers for account-settings

---
+++ handlers/user/update-user-language.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/**
+ * @author Jo-Ries Canino
+ * @description Update User Language
+ */
+
+const lib = require('../../lib');
+
+/**
+ * Validation of req.body, req, param,
+ * and req.query
+ * @param {any} req request object
+ * @param {any} res response object
+ * @param {any} next next object
+ * @returns {next} returns the next handler - success response
+ * @returns {rpc} returns the validation error - failed response
+ */
+function validateParams (req, res, next) {
+  let bodySchema = {
+    language: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Language'
+      },
+    }
+  };
+
+  req.checkBody(bodySchema);
+  return req.getValidationResult()
+  .then(validationErrors => {
+    if (validationErrors.array().length !== 0) {
+      return res.status(400)
+      .send(new lib.rpc.ValidationError(validationErrors.array()));
+    }
+
+    return next();
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+  });
+}
+
+function updateUserLanguage (req, res, next) {
+  let user = req.$scope.user;
+  let language = req.$params.language;
+
+  return req.db.user.update({
+    language: language
+  }, {
+    where: {
+      id: {
+        [req.Op.eq]: user.id
+      }
+    }
+  })
+  .then(user => {
+    next();
+    return user;
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+
+    req.log.error({
+      err: error.message
+    }, 'user.update Error - update-user-language');
+  });
+}
+
+/**
+ * Response data to client
+ * @param {any} req request object
+ * @param {any} res response object
+ * @returns {any} body response object
+ */
+function response (req, res) {
+  let body = {
+    status: 'SUCCESS',
+    status_code: 0,
+    http_code: 200
+  };
+
+  res.status(200).send(body);
+}
+
+module.exports.validateParams = validateParams;
+module.exports.logic = updateUserLanguage;
+module.exports.response = response;
