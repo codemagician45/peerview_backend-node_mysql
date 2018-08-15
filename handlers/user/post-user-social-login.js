fee6bcb2bc1bb7d56545849bad19a6a9be095b5c
Jo-Ries Canino <canino_jories@hotmail.com>
2018-08-15 14:56:08 +0800

    Add getUserProfile in social login

--- handlers/user/post-user-social-login.js
+++ handlers/user/post-user-social-login.js
@@ -183,6 +183,55 @@ function saveOrUpdateUser (req, res, next) {
   }
 }
 
+const getUserProfile = (req, res, next) => {
+  const userId = req.$scope.user.id;
+  const sequelize = req.db.user.sequelize;
+  return req.db.user.findOne({
+    include: [{
+      model: req.db.userInterest,
+      include: [{
+        model: req.db.interest
+      }]
+    }, {
+      model: req.db.userType
+    }, {
+      model: req.db.userCourse,
+      include: {
+        model: req.db.course
+      }
+    }, {
+      model: req.db.userFollower,
+      as: 'followee',
+      attributes: []
+    }, {
+      model: req.db.userCredits,
+      attributes: [
+        [sequelize.fn('SUM',
+          sequelize.col('credits')), 'totalCredits'],
+      ],
+    }],
+    where: {
+      id: {
+        [req.Op.eq]: userId
+      }
+    }
+  })
+  .then(user => {
+    req.$scope.user = user;
+    next();
+    /**
+     * This would a return coming from the top then
+     * so that we can eliminate this error
+     * http://goo.gl/rRqMUw
+     */
+    return user;
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+  });
+};
+
 /**
  * Response data to client
  * @param {any} req request object
@@ -204,4 +253,5 @@ function response (req, res) {
 module.exports.validateParams = validateParams;
 module.exports.findUser = findUser;
 module.exports.saveOrUpdateUser = saveOrUpdateUser;
+module.exports.getUserProfile = getUserProfile;
 module.exports.response = response;
