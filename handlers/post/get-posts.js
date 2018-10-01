47110484e5ef7d1785826383afcdfb270b2313c1
Jo-Ries Canino <canino_jories@hotmail.com>
2018-10-01 23:25:17 +0800

    Add getIfUserAlreadyVoted as part of the running function

--- handlers/post/get-posts.js
+++ handlers/post/get-posts.js
@@ -153,6 +153,7 @@ function getPosts (req, res, next) {
     return req.db.post.prototype.getPOSTREPLY(posts, req.db)
     .then(() => req.db.post.prototype.getATTACHMENTS(posts))
     .then(() => req.db.post.prototype.getPOSTPOLLOPTIONS(posts, req.db))
+    .then(() => req.db.post.prototype.getIfUserAlreadyVoted(posts, user.id))
     .then(() => req.db.post.prototype.getPOSTSHARE(posts, req))
     .then(() => req.db.post.prototype.getPOSTLIKES(posts, req));
   })
