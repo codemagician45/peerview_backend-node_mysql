3feab408d0b18ce6653d8165526d9ab50fde0901
Jo-Ries Canino <canino_jories@hotmail.com>
2018-10-18 14:02:03 +0800

    Add GET and POST related endpoing under message

---
+++ handlers/message/get-message-list.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/**
+ * @author Jo-Ries Canino
+ * @description POST Message
+ */
+
+const lib = require('../../lib');
+
+/**
+ * Initialized the schema Object
+ */
+const querySchema = {
+  limit: { in: ['query'],
+    optional: true,
+    isInt: {
+      errorMessage: 'Invalid Resource: Limit'
+    }
+  },
+  offset: { in: ['query'],
+    optional: true,
+    isInt: {
+      errorMessage: 'Invalid Resource: Offset'
+    }
+  }
+};
+
+const getMessageList = (req, res, next) => {
+  let user = req.$scope.user;
+  let offset = req.$params.offset;
+  let limit = req.$params.limit;
+
+  req.db
+  .message
+  .findAll({
+    include: [{
+      model: req.db.message,
+      as: 'messages',
+      limit: 1,
+      order: [
+        ['createdAt', 'DESC']
+      ],
+    }],
+    where: {
+      parentId: null,
+      [req.Op.or]: [{
+        fromId: user.id
+      }, {
+        toId: user.id
+      }]
+    },
+    order: [
+      ['createdAt', 'DESC']
+    ],
+    offset: !offset ? 0 : parseInt(offset),
+    limit: !limit ? 10 : parseInt(limit)
+  })
+  .then((messageList) => {
+    req.$scope.messageList = messageList;
+
+    next();
+    return messageList;
+  })
+  .catch((error) => {
+    req.log.error({
+      error: error
+    }, 'handlers.message get-message-list-b-parent-id [message.findAll] Error');
+
+    return res.status(lib.httpCodes.SERVER_ERROR)
+    .send(new lib.rpc.InternalError(error));
+  });
+};
+
+/**
+ * Response data to client
+ * @param {any} req request object
+ * @param {any} res response object
+ * @returns {any} body response object
+ */
+const response = (req, res) => {
+  let messageList = req.$scope.messageList;
+  let body = lib.response.createOk(messageList);
+
+  res.status(lib.httpCodes.OK).send(body);
+};
+
+module.exports.querySchema = querySchema;
+module.exports.logic = getMessageList;
+module.exports.response = response;
