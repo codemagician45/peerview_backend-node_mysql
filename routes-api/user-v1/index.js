625713614e2846264083a8417ca15ac230351a23
Jo-Ries Canino <canino_jories@hotmail.com>
2018-10-21 16:30:21 +0800

    Add follow endpoint in user-v1

--- routes-api/user-v1/index.js
+++ routes-api/user-v1/index.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const router = new express.Router();
 const searchApi = require('./search');
+const followApi = require('./follow');
 
 const userApi = (api) => {
   api.use('/user', router);
 
   searchApi(router);
+  followApi(router);
 };
 
 module.exports = userApi;
