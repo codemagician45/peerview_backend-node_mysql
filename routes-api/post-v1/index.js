a2a938bacbd07be0f4be934c14bebaed3cf2e769
Jo-Ries Canino <canino_jories@hotmail.com>
2018-10-27 12:31:25 +0800

    Add list api endpont for postv1

--- routes-api/post-v1/index.js
+++ routes-api/post-v1/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 const router = new express.Router();
 const defaultApi = require('./default');
 const replyApi = require('./reply');
+const listApi = require('./list');
 
 const postApi = (api) => {
   api.use('/post', router);
 
   defaultApi(router);
   replyApi(router);
+  listApi(router);
 };
 
 module.exports = postApi;
