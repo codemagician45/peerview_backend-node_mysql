2d91090e43dcc25587719750b1b5a1bf506e8218
Jo-Ries Canino <canino_jories@hotmail.com>
2018-11-05 07:52:37 +0800

    Add reply model association

--- data-models/reply.js
+++ data-models/reply.js
@@ -23,6 +23,14 @@ module.exports = function (sequelize, dataTypes) {
   Reply.associate = function (models) {
     this.belongsTo(models.postv1);
     this.belongsTo(models.user);
+    this.belongsTo(models.reply, {
+      foreignKey: 'quoteReplyId',
+      as: 'quoteReply'
+    });
+    this.belongsTo(models.user, {
+      foreignKey: 'tagUserId',
+      as: 'tagUser'
+    });
   };
 
   return Reply;
