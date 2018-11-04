2ed3650235b0da33cf398272488672bee6cb7317
Jo-Ries Canino <canino_jories@hotmail.com>
2018-11-05 07:58:43 +0800

    Require needed api endpoint in postv1

--- routes-api/post-v1/index.js
+++ routes-api/post-v1/index.js
@@ -3,15 +3,19 @@
 const express = require('express');
 const router = new express.Router();
 const defaultApi = require('./default');
-const replyApi = require('./reply');
 const listApi = require('./list');
+const replyApi = require('./reply');
+const likeApi = require('./like');
+const reportApi = require('./report');
 
 const postApi = (api) => {
   api.use('/post', router);
 
   defaultApi(router);
-  replyApi(router);
   listApi(router);
+  replyApi(router);
+  likeApi(router);
+  reportApi(router);
 };
 
 module.exports = postApi;
