b02043423bdbad2535ab8d74c6b48020b1d33bf1
BL <bhushankumar.lilapara@gmail.com>
2019-01-12 15:07:38 +0530

    Modified get users using couseId query

--- handlers/user/get-users-search-via-course-id.js
+++ handlers/user/get-users-search-via-course-id.js
@@ -2,10 +2,11 @@ const lib = require('../../lib');
 
 function getUsers (req, res, next) {
   let courseId = req.params.courseId;
+  const dataBaseTable = process.env.PEERSVIEW_DATABASE || 'peersview' ;
    return req.db.userCourse.sequelize.query(`SELECT UC.id as userCourseId, UC.courseId as courseId, 
    U.id as id, U.firstName as firstName, U.lastName as lastName, U.email as email, U.schoolName as schoolName,
-   U.profilePicture as profilePicture, U.socialImage as socialImage FROM peersview.user_course as UC, 
-    peersview.user as U WHERE UC.userId = U.id and UC.courseId = ${courseId}`,
+   U.profilePicture as profilePicture, U.socialImage as socialImage FROM ${dataBaseTable}.user_course as UC, 
+    ${dataBaseTable}.user as U WHERE UC.userId = U.id and UC.courseId = ${courseId}`,
      { type: req.db.userCourse.sequelize.QueryTypes.SELECT})
     .then(users => {
       req.$scope.courseUsers = users;
