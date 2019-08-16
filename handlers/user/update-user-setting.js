cfd531bf66d1d0135d9b5d80baeaded9f07b34ee
classy-dev <gwolf.bond97@gmail.com>
2019-08-16 13:28:56 -0700

    api for account general setting

---
+++ handlers/user/update-user-setting.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/**
+ * @author Jo-Ries Canino
+ * @description Update User Name
+ */
+
+const lib = require('../../lib');
+
+function updateGeneralSetting (req, res, next) {
+
+  let bodySchema = {
+    firstName: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: First Name'
+      },
+    },
+    lastName: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Last Name'
+      }
+    },
+    language: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Language'
+      }
+    },
+    birthDate: {
+      notEmpty: {
+        errorMessage: 'Missing Resource: Birth Date'
+      }
+    }
+  };
+
+  req.checkBody(bodySchema);
+  return req.getValidationResult()
+  .then(validationErrors => {
+    if (validationErrors.array().length !== 0) {
+      return res.status(400)
+      .send(new lib.rpc.ValidationError(validationErrors.array()));
+    }
+
+    let user = req.$scope.user;
+    let firstName = req.$params.firstName;
+    let lastName = req.$params.lastName;
+    let language = req.$params.language;
+    let birthDate = req.$params.birthDate;
+
+    req.db.user.update({
+      firstName: firstName,
+      lastName: lastName,
+      language: language,
+      birthDate: birthDate
+    }, {
+      where: {
+        id: {
+          [req.Op.eq]: user.id
+        }
+      }
+    })
+    .then(user => {
+      let body = {
+        status: 'SUCCESS',
+        status_code: 0,
+        http_code: 200,
+        data: user
+      };
+    
+      res.status(200).send(body);
+    })
+    .catch(error => {
+      res.status(500)
+      .send(new lib.rpc.InternalError(error));
+  
+      req.log.error({
+        err: error.message
+      }, 'user.update Error - update-user-name');
+    });
+  })
+  .catch(error => {
+    res.status(500)
+    .send(new lib.rpc.InternalError(error));
+  });
+}
+
+module.exports.updateGeneralSetting = updateGeneralSetting;
