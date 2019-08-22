5bc8b64660d4d32627cc28d45e4e448289245613
classy-dev <gwolf.bond97@gmail.com>
2019-08-21 20:55:51 -0700

    add column for attachment table

--- lib/migration/alter-column.sql
+++ lib/migration/alter-column.sql
@@ -1,2 +1,11 @@
 ALTER TABLE message
 ADD COLUMN isRead TINYINT(1) AFTER detail;
+
+ALTER TABLE `attachment` 
+ADD COLUMN `replyId` INT(10) UNSIGNED NULL AFTER `eventPostId`;
+
+ALTER TABLE `attachment` 
+ADD CONSTRAINT `attachment_ibfk_7`
+  FOREIGN KEY (`replyId`)
+  REFERENCES `reply` (`id`)
+  ON DELETE SET NULL ON UPDATE CASCADE;
